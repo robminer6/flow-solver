75cae9fc8fe1b16191a8c096a30a41a819227e9d
Rob Miner <robminer@gmail.com>
2022-10-26 14:45:07 -0400

    Added eslint support

--- src/flow.tsx
+++ src/flow.tsx
@@ -1,126 +1,201 @@
 enum FlowSpace {
     Empty = "Empty",
     YellowCircle = "YellowCircle",
-    RedCircle= "RedCircle",
-    BlueCircle= "BlueCircle",
-    CyanCircle= "CyanCircle",
-    OrangeCircle= "OrangeCircle",
-    ForestCircle= "ForestCircle",
-    LimeCircle= "LimeCircle",
-    MagentaCircle= "MagentaCircle",
-    PurpleCircle= "PurpleCircle",
-    Yellow= "Yellow",
-    Red= "Red",
-    Blue= "Blue",
-    Cyan= "Cyan",
-    Orange= "Orange",
-    Forest= "Forest",
-    Lime= "Lime",
-    Magenta= "Magenta",
-    Purple= "Purple"
-};
-
-function isCircle(entry: FlowSpace){
-        if (entry === FlowSpace.OrangeCircle){
-            return FlowSpace.Orange;
-        }
-        if (entry === FlowSpace.RedCircle){
-            return FlowSpace.Red;
-        }
-        if (entry === FlowSpace.BlueCircle){
-            return FlowSpace.Blue;
-        }
-        if (entry === FlowSpace.ForestCircle){
-            return FlowSpace.Forest;
-        }
-        if (entry === FlowSpace.YellowCircle){
-            return FlowSpace.Yellow;
-        }
-        if (entry === FlowSpace.CyanCircle){
-            return FlowSpace.Cyan;
-        }
-        if (entry === FlowSpace.LimeCircle){
-            return FlowSpace.Lime;
-        }
-        if (entry === FlowSpace.MagentaCircle){
-            return FlowSpace.Magenta;
-        }
-        if (entry === FlowSpace.PurpleCircle){
-            return FlowSpace.Purple;
-        }
-        return FlowSpace.Empty; //Failure condition
+    RedCircle = "RedCircle",
+    BlueCircle = "BlueCircle",
+    CyanCircle = "CyanCircle",
+    OrangeCircle = "OrangeCircle",
+    ForestCircle = "ForestCircle",
+    LimeCircle = "LimeCircle",
+    MagentaCircle = "MagentaCircle",
+    PurpleCircle = "PurpleCircle",
+    Yellow = "Yellow",
+    Red = "Red",
+    Blue = "Blue",
+    Cyan = "Cyan",
+    Orange = "Orange",
+    Forest = "Forest",
+    Lime = "Lime",
+    Magenta = "Magenta",
+    Purple = "Purple",
 }
 
-export default class FlowGame{
+function isCircle(entry: FlowSpace) {
+    if (entry === FlowSpace.OrangeCircle) {
+        return FlowSpace.Orange;
+    }
+    if (entry === FlowSpace.RedCircle) {
+        return FlowSpace.Red;
+    }
+    if (entry === FlowSpace.BlueCircle) {
+        return FlowSpace.Blue;
+    }
+    if (entry === FlowSpace.ForestCircle) {
+        return FlowSpace.Forest;
+    }
+    if (entry === FlowSpace.YellowCircle) {
+        return FlowSpace.Yellow;
+    }
+    if (entry === FlowSpace.CyanCircle) {
+        return FlowSpace.Cyan;
+    }
+    if (entry === FlowSpace.LimeCircle) {
+        return FlowSpace.Lime;
+    }
+    if (entry === FlowSpace.MagentaCircle) {
+        return FlowSpace.Magenta;
+    }
+    if (entry === FlowSpace.PurpleCircle) {
+        return FlowSpace.Purple;
+    }
+    return FlowSpace.Empty; // Failure condition
+}
+
+export default class FlowGame {
     grid: FlowSpace[][];
-    constructor(){
+
+    constructor() {
         this.grid = [
-            [FlowSpace.RedCircle, FlowSpace.Empty, FlowSpace.ForestCircle, FlowSpace.Empty, FlowSpace.YellowCircle],
-            [FlowSpace.Empty, FlowSpace.Empty, FlowSpace.BlueCircle, FlowSpace.Empty, FlowSpace.OrangeCircle],
-            [FlowSpace.Empty, FlowSpace.Empty, FlowSpace.Empty, FlowSpace.Empty, FlowSpace.Empty],
-            [FlowSpace.Empty, FlowSpace.ForestCircle, FlowSpace.Empty, FlowSpace.YellowCircle, FlowSpace.Empty],
-            [FlowSpace.Empty, FlowSpace.RedCircle, FlowSpace.BlueCircle, FlowSpace.OrangeCircle, FlowSpace.Empty]
-        ]; //Unsolved default puzzle
+            [
+                FlowSpace.RedCircle,
+                FlowSpace.Empty,
+                FlowSpace.ForestCircle,
+                FlowSpace.Empty,
+                FlowSpace.YellowCircle,
+            ],
+            [
+                FlowSpace.Empty,
+                FlowSpace.Empty,
+                FlowSpace.BlueCircle,
+                FlowSpace.Empty,
+                FlowSpace.OrangeCircle,
+            ],
+            [
+                FlowSpace.Empty,
+                FlowSpace.Empty,
+                FlowSpace.Empty,
+                FlowSpace.Empty,
+                FlowSpace.Empty,
+            ],
+            [
+                FlowSpace.Empty,
+                FlowSpace.ForestCircle,
+                FlowSpace.Empty,
+                FlowSpace.YellowCircle,
+                FlowSpace.Empty,
+            ],
+            [
+                FlowSpace.Empty,
+                FlowSpace.RedCircle,
+                FlowSpace.BlueCircle,
+                FlowSpace.OrangeCircle,
+                FlowSpace.Empty,
+            ],
+        ]; // Unsolved default puzzle
         this.checkComplete();
         this.grid = [
-            [FlowSpace.RedCircle, FlowSpace.Forest, FlowSpace.ForestCircle, FlowSpace.Yellow, FlowSpace.YellowCircle],
-            [FlowSpace.Red, FlowSpace.Forest, FlowSpace.BlueCircle, FlowSpace.Yellow, FlowSpace.OrangeCircle],
-            [FlowSpace.Red, FlowSpace.Forest, FlowSpace.Blue, FlowSpace.Yellow, FlowSpace.Orange],
-            [FlowSpace.Red, FlowSpace.ForestCircle, FlowSpace.Blue, FlowSpace.YellowCircle, FlowSpace.Orange],
-            [FlowSpace.Red, FlowSpace.RedCircle, FlowSpace.BlueCircle, FlowSpace.OrangeCircle, FlowSpace.Orange]
+            [
+                FlowSpace.RedCircle,
+                FlowSpace.Forest,
+                FlowSpace.ForestCircle,
+                FlowSpace.Yellow,
+                FlowSpace.YellowCircle,
+            ],
+            [
+                FlowSpace.Red,
+                FlowSpace.Forest,
+                FlowSpace.BlueCircle,
+                FlowSpace.Yellow,
+                FlowSpace.OrangeCircle,
+            ],
+            [
+                FlowSpace.Red,
+                FlowSpace.Forest,
+                FlowSpace.Blue,
+                FlowSpace.Yellow,
+                FlowSpace.Orange,
+            ],
+            [
+                FlowSpace.Red,
+                FlowSpace.ForestCircle,
+                FlowSpace.Blue,
+                FlowSpace.YellowCircle,
+                FlowSpace.Orange,
+            ],
+            [
+                FlowSpace.Red,
+                FlowSpace.RedCircle,
+                FlowSpace.BlueCircle,
+                FlowSpace.OrangeCircle,
+                FlowSpace.Orange,
+            ],
         ]; // Solved default puzzle
         this.checkComplete();
     }
 
-    checkCirclePath(i: number, j: number, line: FlowSpace){
-        let circle: FlowSpace = this.grid[i][j];
+    checkCirclePath(row: number, col: number, line: FlowSpace) {
+        let i = row;
+        let j = col;
+        const circle: FlowSpace = this.grid[i][j];
         let pasti = i;
         let pastj = j;
-        while(this.grid[i][j] !== circle || (i === pasti && j === pastj)){
+        while (this.grid[i][j] !== circle || (i === pasti && j === pastj)) {
             let nexti = -1;
             let nextj = -1;
-            if (i !== 0 && pasti !== i-1){
-                //Check square above
-                if (this.grid[i-1][j] === line || this.grid[i-1][j] === circle){
-                    nexti = i-1;
+            if (i !== 0 && pasti !== i - 1) {
+                // Check square above
+                if (
+                    this.grid[i - 1][j] === line ||
+                    this.grid[i - 1][j] === circle
+                ) {
+                    nexti = i - 1;
                     nextj = j;
                 }
             }
-            if (i !== this.grid.length-1 && pasti !== i+1){
-                //Check square below
-                if (this.grid[i+1][j] === line|| this.grid[i+1][j] === circle){
-                    if (nexti !== -1){
-                        console.log("No 2x2 allowed! Space:" + i + " " + j);
+            if (i !== this.grid.length - 1 && pasti !== i + 1) {
+                // Check square below
+                if (
+                    this.grid[i + 1][j] === line ||
+                    this.grid[i + 1][j] === circle
+                ) {
+                    if (nexti !== -1) {
+                        console.log(`No 2x2 allowed! Space:${i} ${j}`);
                         return false;
                     }
-                    nexti = i+1;
+                    nexti = i + 1;
                     nextj = j;
                 }
             }
-            if (j !== 0 && pastj !== j-1){
-                //Check square left
-                if (this.grid[i][j-1] === line || this.grid[i][j-1] === circle){
-                    if (nexti !== -1){
-                        console.log("No 2x2 allowed! Space:" + i + " " + j);
+            if (j !== 0 && pastj !== j - 1) {
+                // Check square left
+                if (
+                    this.grid[i][j - 1] === line ||
+                    this.grid[i][j - 1] === circle
+                ) {
+                    if (nexti !== -1) {
+                        console.log(`No 2x2 allowed! Space:${i} ${j}`);
                         return false;
                     }
                     nexti = i;
-                    nextj = j-1;
+                    nextj = j - 1;
                 }
             }
-            if (j !== this.grid[0].length && pastj !== j+1){
-                //Check square right
-                if (this.grid[i][j+1] === line || this.grid[i][j+1] === circle){
-                    if (nexti !== -1){
-                        console.log("No 2x2 allowed! Space:" + i + " " + j);
+            if (j !== this.grid[0].length && pastj !== j + 1) {
+                // Check square right
+                if (
+                    this.grid[i][j + 1] === line ||
+                    this.grid[i][j + 1] === circle
+                ) {
+                    if (nexti !== -1) {
+                        console.log(`No 2x2 allowed! Space:${i} ${j}`);
                         return false;
                     }
                     nexti = i;
-                    nextj = j+1;
+                    nextj = j + 1;
                 }
             }
-            if (nexti === -1){
-                console.log("Couldn't find next space! Space: " + i + " " + j);
+            if (nexti === -1) {
+                console.log(`Couldn't find next space! Space: ${i} ${j}`);
                 return false;
             }
             pasti = i;
@@ -131,14 +206,14 @@ export default class FlowGame{
         return true;
     }
 
-    checkComplete(){
+    checkComplete() {
         console.log("Checking grid.");
         this.printGrid();
-        for(let i = 0; i < this.grid.length; i++){
-            for (let j = 0; j < this.grid[0].length; j++){
-                let k: FlowSpace = isCircle(this.grid[i][j]);
-                if (k !== FlowSpace.Empty){
-                    if (!this.checkCirclePath(i, j, k)){
+        for (let i = 0; i < this.grid.length; i += 1) {
+            for (let j = 0; j < this.grid[0].length; j += 1) {
+                const k: FlowSpace = isCircle(this.grid[i][j]);
+                if (k !== FlowSpace.Empty) {
+                    if (!this.checkCirclePath(i, j, k)) {
                         return false;
                     }
                 }
@@ -147,22 +222,24 @@ export default class FlowGame{
         console.log("We done did it gamers!");
         return true;
     }
-    printGrid(){
-        for(let i = 0; i < this.grid.length; i++){
+
+    printGrid() {
+        for (let i = 0; i < this.grid.length; i += 1) {
             let linetoprint = "";
-            for(let j = 0; j < this.grid[0].length; j++){
-                if (isCircle(this.grid[i][j]) !== FlowSpace.Empty){
-                    linetoprint += this.grid[i][j].toString().substring(0,3) + "C ";
-                }
-                else{
-                    linetoprint += this.grid[i][j].toString().substring(0,3) + "  ";
+            for (let j = 0; j < this.grid[0].length; j += 1) {
+                if (isCircle(this.grid[i][j]) !== FlowSpace.Empty) {
+                    linetoprint += `${this.grid[i][j]
+                        .toString()
+                        .substring(0, 3)}C `;
+                } else {
+                    linetoprint += `${this.grid[i][j]
+                        .toString()
+                        .substring(0, 3)}  `;
                 }
             }
             console.log(linetoprint);
         }
     }
-    solve(){
 
-    }
-
+    // solve() {}
 }
