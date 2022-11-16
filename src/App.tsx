ec82885f8a0fc2297e234470c25934f3161f1e7a
Matthew DeBacker <mdebackr@umich.edu>
2022-11-16 15:07:13 -0500

    layout.

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ function Item(props: BoxProps) {
           border: '1px solid',
           borderColor: 'white',
           p: 1,
-          height: '50px',
-          width: '50px',
+          width: '40px',
+          height: '40px',
           fontSize: '0.875rem',
           fontWeight: '700',
           ...sx,
@@ -26,55 +26,71 @@ function Item(props: BoxProps) {
 function App() {
     const [gridHeight, setHeight] = useState(5);
     const [gridWidth, setWidth] = useState(5);
+    const [color, setColor] = useState("Red");
+    const colorList = ["yellow", "red", "blue", "cyan", "orange", "green", "lime", "magenta", "purple"];
+    const colorMap = {
+        "yellow": "#ffff00",
+        "red": "#ff0000",
+        "blue": "#0000ff",
+        "cyan": "#00ffff",
+        "orange": "#ffa500",
+        "green": "#008000",
+        "lime": "#00ff00",
+        "magenta": "#ff00ff",
+        "purple": "#A020F0",
+    }
     
     const handleHeight = (event: any) => {
         setHeight(event.target.value);
     }
 
     const handleWidth = (event: any) => {
-
         setWidth(event.target.value);
+    }
+
+    const handleColor = (event: any) => {
+        setColor(event.target.value);
     }
     
     return (
         <div className="App">
-            <header className="App-header">
-            <select value={gridWidth} onChange={handleWidth}>
-                <option value={5}>5</option>
-                <option value={6}>6</option>
-                <option value={7}>7</option>
-                <option value={8}>8</option>
-                <option value={9}>9</option>
-                <option value={11}>11</option>
-                <option value={12}>12</option>
-                <option value={13}>13</option>
-                <option value={14}>14</option>
-                <option value={15}>15</option>
-            </select>
-            <select value={gridHeight} onChange={handleHeight}>
-                <option value={5}>5</option>
-                <option value={6}>6</option>
-                <option value={7}>7</option>
-                <option value={8}>8</option>
-                <option value={9}>9</option>
-            </select>
-            <p>
-                {gridWidth} {gridHeight}
-            </p>
+            <div className="Outer-Box">
+                <div className="Grid-select">
+                        <select className="Grid-select-left" value={gridWidth} onChange={handleWidth}>
+                            <option value={5}>5</option>
+                            <option value={6}>6</option>
+                            <option value={7}>7</option>
+                            <option value={8}>8</option>
+                            <option value={9}>9</option>
+                            <option value={11}>11</option>
+                            <option value={12}>12</option>
+                            <option value={13}>13</option>
+                            <option value={14}>14</option>
+                            <option value={15}>15</option>
+                        </select>
+                        <select className="Grid-selct-right" value={gridHeight} onChange={handleHeight}>
+                            <option value={5}>5</option>
+                            <option value={6}>6</option>
+                            <option value={7}>7</option>
+                            <option value={8}>8</option>
+                            <option value={9}>9</option>
+                        </select>
+                </div>
 
-            <Box
-                sx={{
-                display: 'grid',
-                gridTemplateColumns: `repeat(${gridWidth}, 1fr)`,
-                }}
-            >
-                {
-                    Array.from(Array(gridHeight * gridWidth)).map((_, index) => (
-                        <Item>{index}</Item>
-                    ))
-                }
-            </Box>
-            </header>
+                <Box
+                    sx={{
+                    display: 'grid',
+                    gridTemplateColumns: `repeat(${gridWidth}, 1fr)`,
+                    }}
+                >
+                    {
+                        Array.from(Array(gridHeight * gridWidth)).map((_, index) => (
+                            // eslint-disable-next-line no-template-curly-in-string
+                            <Item>{index}</Item>
+                        ))
+                    }
+                </Box>
+            </div>
         </div>
     );
 }
